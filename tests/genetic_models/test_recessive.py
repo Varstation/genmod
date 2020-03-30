f489b3a17508671c2d6ffdd17f246ebc163467ec
George Carvalho <george@genomika.com.br>
2020-03-30 15:23:08 -0300

    Add more tests

--- tests/genetic_models/test_recessive.py
+++ tests/genetic_models/test_recessive.py
@@ -19,7 +19,7 @@ def get_family(family_file = None, family_lines = None):
 
 ################# Test affected ###############
 
-def test_dominant_affected_homozygote_male():
+def test_recessive_affected_homozygote_male():
     """Test an affected homozygote male"""
     
     family_lines = [
@@ -37,6 +37,44 @@ def test_dominant_affected_homozygote_male():
         family = family,
         strict = False
     ) == True
+
+def test_recessive_affected_het_male():
+    """Test a sick male
+    """
+    family_lines = [
+        "#FamilyID\tSampleID\tFather\tMother\tSex\tPhenotype\n",
+        "1\tproband\t0\t0\t1\t2\n"
+    ]
+    
+    family = get_family(family_lines=family_lines)
+    
+    recessive_variant = {'genotypes': {}}
+    recessive_variant['genotypes']['proband'] = Genotype(**{'GT':'0/1'})
+    
+    assert check_recessive(
+        variant = recessive_variant,
+        family = family,
+        strict = False
+    ) == False
+
+def test_recessive_affected_male_ref_call():
+    """Test an affected ref call male"""
+    
+    family_lines = [
+        "#FamilyID\tSampleID\tFather\tMother\tSex\tPhenotype\n",
+        "1\tproband\t0\t0\t1\t2\n"
+    ]
+    
+    family = get_family(family_lines=family_lines)
+    
+    homozygote_variant = {'genotypes': {}}
+    homozygote_variant['genotypes']['proband'] = Genotype(**{'GT':'0/0'})
+    
+    assert check_recessive(
+        variant = homozygote_variant,
+        family = family,
+        strict = False
+    ) == False
 
 ############### Test family ##############
 
